b9175435d863941b3cd76890464011bd0d2a1161
mdimran077 <mdimranrazzaque077@gmail.com>
2026-07-06 12:14:12 +0530

    GitHub Pages deployment setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/react-vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/intern-portal/', // <-- Apne repository ka naam yahan dalein (dono taraf / zaroori hai)
 })
