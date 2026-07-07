961cb529f6c516783480077786b31c22e8e98ea3
mdimran077 <mdimranrazzaque077@gmail.com>
2026-07-07 10:45:33 +0530

    fixed tailwind and build configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // <-- Isko sahi kar lijiye
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/intern-portal/', 
+  base: './', // '/' ke bajaye './' use karein
 })
